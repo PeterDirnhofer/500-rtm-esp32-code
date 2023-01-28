bb5e96675e9e5f2591640103b9ca6e9cc062363b
Peter Dirnhofer <peter.dirnhofer@t-online.de>
2023-01-28 16:33:14 +0100

    DAC X Y Z ok

--- src/UsbPcInterface.cpp
+++ src/UsbPcInterface.cpp
@@ -1,6 +1,5 @@
 #include <UsbPcInterface.h>
 
-
 // https://github.com/espressif/esp-idf/blob/30e8f19f5ac158fc57e80ff97c62b6cc315aa337/examples/peripherals/uart/uart_async_rxtxtasks/main/uart_async_rxtxtasks_main.c
 
 static const char *TAG = "UsbPcInterface";
@@ -159,8 +158,44 @@ esp_err_t UsbPcInterface::sendData()
     return ESP_OK;
 }
 
+int16_t normToMaxMin(long int invalue)
+{
+    if (invalue < 0)
+        return 0;
+
+    if (invalue > ADC_VALUE_MAX)
+        return ADC_VALUE_MAX;
+
+    return (int16_t)invalue;
+}
+
 esp_err_t UsbPcInterface::mUpdateTip(string s)
 {
+
+    char *cstr = new char[s.length() + 1];
+    strcpy(cstr, s.c_str());
+    vector<string> arguments;
+
+    // how many comma are in string
+    int numberOfValues = 1;
+    for (int i = 0; i < UsbPcInterface::mUsbReceiveString.length(); i++)
+        if (cstr[i] == ',')
+            numberOfValues++;
+
+    char *p = strtok(cstr, ",");
+
+    arguments.clear();
+    while (p != 0)
+    {
+        // printf("%s\n", p);
+        char buffer[20];
+        sprintf(buffer, "%s", p);
+        arguments.push_back(buffer);
+        // UsbPcInterface::send("Add %s\n", buffer);
+        p = strtok(NULL, ",");
+    }
+
+    // UsbPcInterface::send("size of arguments %d\n", arguments.size());
 
     if (UsbPcInterface::adjustIsActive == false)
     {
@@ -169,14 +204,75 @@ esp_err_t UsbPcInterface::mUpdateTip(string s)
         return ESP_ERR_INVALID_ARG;
     }
 
-    int l = strlen(s.c_str());
-    ESP_LOGI(TAG, "length TIP command: %d", l);
-    if ((l < 5) || s[3] != ',')
+    int l = arguments.size();
+
+    // TIP,?
+    if (l == 2) //  && (strcmp(arguments[1].c_str(), "ADJUST") == 0))
     {
-        ESP_LOGW("mUpdateTip", "No valid parameter for TIP. Needs 'TIP,integer'. Got %s\n", s.c_str());
-        UsbPcInterface::send("No valid parameter for TIP. Needs 'TIP,integer'\n");
+        UsbPcInterface::send("TIP,? detected %s %s\n", arguments[0].c_str(), arguments[1].c_str());
+        if (strcmp(arguments[1].c_str(), "?") == 0)
+        {
+
+            UsbPcInterface::send("Send X Y Z\n");
+        }
+        else
+        {
+            UsbPcInterface::send("No valid parameter for TIP. Only valid in ADJUST mode\n");
+            return ESP_ERR_INVALID_ARG;
+        }
+    }
+
+    if (l != 4)
+    {
+        UsbPcInterface::send("No valid parameter for 'TIP,X,Y,Z'. X is no integer \n");
         return ESP_ERR_INVALID_ARG;
     }
+
+    // TIP,X,Y,Z
+    if (l == 4)
+    {
+        int16_t x = 0;
+        int16_t y = 0;
+        int16_t z = 0;
+
+        char *endPtr;
+        long int xl = strtol(arguments[1].c_str(), &endPtr, 10);
+
+        if (strlen(endPtr) > 0)
+        {
+            UsbPcInterface::send("No valid parameter for 'TIP,X,Y,Z'. X is no integer \n");
+            return ESP_ERR_INVALID_ARG;
+        }
+        x = normToMaxMin(xl);
+
+        long int yl = strtol(arguments[2].c_str(), &endPtr, 10);
+        if (strlen(endPtr) > 0)
+        {
+            UsbPcInterface::send("No valid parameter for 'TIP,X,Y,Z'. X is no integer \n");
+            return ESP_ERR_INVALID_ARG;
+        }
+        y = normToMaxMin(yl);
+
+        long int zl = strtol(arguments[3].c_str(), &endPtr, 10);
+        if (strlen(endPtr) > 0)
+        {
+            UsbPcInterface::send("No valid parameter for 'TIP,X,Y,Z'. X is no integer \n");
+            return ESP_ERR_INVALID_ARG;
+        }
+        z = normToMaxMin(zl);
+
+        UsbPcInterface::send("detected x y z %d %d %d\n", x, y, z);
+        currentXDac = x;
+        currentYDac = y;
+        currentZDac = z;
+
+        vTaskResume(handleVspiLoop); // realize newZ. Will suspend itself
+
+        return ESP_OK;
+    }
+
+    UsbPcInterface::send("No valid parameter for TIP. Only valid in ADJUST mode\n");
+    return ESP_ERR_INVALID_ARG;
 
     if (s[3] != ',')
     {
@@ -310,6 +406,15 @@ int UsbPcInterface::getWorkingMode()
 vector<string> UsbPcInterface::getParametersFromPc()
 {
     return this->mParametersVector;
+}
+
+vector<string> s_getParametersFromPC()
+{
+    vector<string> hs;
+
+    hs.push_back("EINS");
+
+    return hs;
 }
 
 void UsbPcInterface::printErrorMessageAndRestart(string error_string)
